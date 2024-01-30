ac03f8f268b3a999c40dc3f707b35760d1a6691b
GuillermoHerrera <guillhermoherrera@gmail.com>
2024-01-30 16:43:00 -0600

    uuid device_info

--- lib/services/http_services.dart
+++ lib/services/http_services.dart
@@ -1,12 +1,36 @@
+import 'dart:io';
+
+import 'package:device_info_plus/device_info_plus.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:http/http.dart' as http;
 class HttpService{
+
+  final _storage = const FlutterSecureStorage();
+
+  Future<String> getDeviceUniqueId() async {
+    var deviceIdentifier = '';
+    var deviceInfo = DeviceInfoPlugin();
+    
+    if (Platform.isAndroid) {
+      var androidInfo = await deviceInfo.androidInfo;
+      deviceIdentifier = androidInfo.id;
+    } else if (Platform.isIOS) {
+      var iosInfo = await deviceInfo.iosInfo;
+      deviceIdentifier = iosInfo.identifierForVendor!;
+    } else {
+      deviceIdentifier = 'unknown';
+    }
+    return deviceIdentifier;
+  }
   
   Future<String> postRequest(url, body) async{
     String respuesta = '';
+    String? token = await _storage.read(key: 'token');
+    String uuid = await getDeviceUniqueId();
     Map<String, String> headers = {
       'Content-Type': 'application/json',
-      'Authorization': 'Bearer',
-      'UUId': 'test'
+      'Authorization': 'Bearer $token',
+      'UUId': uuid
     };
 
     await http.post(url, headers: headers, body: body).then((value){
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,11 +5,13 @@
 import FlutterMacOS
 import Foundation
 
+import device_info_plus
 import flutter_secure_storage_macos
 import path_provider_foundation
 import video_player_avfoundation
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  DeviceInfoPlusMacosPlugin.register(with: registry.registrar(forPlugin: "DeviceInfoPlusMacosPlugin"))
   FlutterSecureStoragePlugin.register(with: registry.registrar(forPlugin: "FlutterSecureStoragePlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   FVPVideoPlayerPlugin.register(with: registry.registrar(forPlugin: "FVPVideoPlayerPlugin"))
